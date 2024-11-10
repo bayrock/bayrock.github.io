bbd05f8d8c1b469be5c4c026806710bd8527c475
Bailey Chestnut <bayrock@users.noreply.github.com>
2024-11-10 10:22:05 -0500

    Add solidjs support and port Clock component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bayrock.dev',
-    server: { port: 80 }
+  site: 'https://bayrock.dev',
+  server: {
+    port: 80
+  },
+  integrations: [solidJs()]
 });
