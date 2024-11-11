5f0fed7a26ad23beed93cce61dc58e0933406b31
Bailey Chestnut <bayrock@users.noreply.github.com>
2024-11-10 21:34:34 -0500

    Remove solidjs and fix clock logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import solidJs from "@astrojs/solid-js";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bayrock.dev',
   server: {
     port: 80
-  },
-  integrations: [solidJs()]
+  }
 });
