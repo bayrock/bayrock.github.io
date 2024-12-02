41ad39040d93bf8e27baa0fda422e772847890c4
Bailey Chestnut <bayrock@users.noreply.github.com>
2024-12-01 21:01:24 -0500

    Add weeks to getTimeString

--- src/modules/getTimeString.js
+++ src/modules/getTimeString.js
@@ -2,11 +2,11 @@ import getPlural from "./getPlural";
 import getTimeRelative from "./getTimeRelative";
 
 function getTimeString(s) {
-    if (typeof s != "number") return "";
     const { years, months, days, hours, minutes, seconds } = getTimeRelative(s);
 
     const yearsAgo = years > 0 ? getPlural("year", years) : "";
     const monthsAgo = months > 0 ? getPlural("month", months) : "";
+    const weeksAgo = days > 6 ? getPlural("week", Math.floor(days / 7)) : "";
     const daysAgo = days > 0 ? getPlural("day", days) : "";
     const hoursAgo = hours > 0 ? getPlural("hour", hours) : "";
     const minutesAgo = minutes > 0 ? getPlural("minute", minutes) : "";
@@ -17,6 +17,7 @@ function getTimeString(s) {
     if (minutesAgo) timeAgo = minutesAgo;
     if (hoursAgo) timeAgo = hoursAgo;
     if (daysAgo) timeAgo = daysAgo;
+    if (weeksAgo) timeAgo = weeksAgo;
     if (monthsAgo) timeAgo = monthsAgo;
     if (yearsAgo) timeAgo = yearsAgo;
 
